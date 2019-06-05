d10678f162c44ae698f9b5c7f0cea04ce633f07e
Carl Hain <crhain@gmail.com>
2019-06-05 16:23:24 -0500

    Add some html to about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,1 +1,19 @@
-About
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'LSAPP')}}</title>
+
+        <!-- Fonts -->
+        
+
+        <!-- Styles -->
+        
+    </head>
+    <body>
+        <h1>About</h1>
+        <p>This is the about page</p>
+    </body>
+</html>
