61ddd2b95ef8d30aba576dd92c77ac8594b6985e
Carl Hain <crhain@gmail.com>
2019-06-05 16:27:18 -0500

    Add basic services.blade.php to views/pages

---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'LSAPP')}}</title>
+
+        <!-- Fonts -->
+        
+
+        <!-- Styles -->
+        
+    </head>
+    <body>
+        <h1>Services</h1>
+        <p>This is the services page</p>
+    </body>
+</html>
