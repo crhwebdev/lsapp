04578e584a43e79cc2cc212aae54ebc25ac88435
Carl Hain <crhain@gmail.com>
2019-06-05 16:58:18 -0500

    Update about method on PagesController to also pass the variable for the title and use it in the about.blade.php page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,12 +10,14 @@ class PagesController extends Controller
         $title = 'Welcome To Laravel, Fine Denizens of the Interwebs';
 
         // return view('pages.index', compact('title'));
+
         return view('pages.index')->with('title', $title);
 
     }
 
     public function about(){
-        return view('pages.about');
+        $title = 'Out and About';
+        return view('pages.about')->with('title', $title);
     }
 
     public function services(){
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>About</h1>
+<h1>{{$title}}</h1>
   <p>This is the about page</p>
 @endsection 
         
