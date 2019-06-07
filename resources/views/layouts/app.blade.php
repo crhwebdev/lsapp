b7ce5bf6a3f18f0cd3debb5a6e85697b2363ca5a
Carl Hain <crhain@gmail.com>
2019-06-07 12:12:15 -0500

    Add navbar include to app layout file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,14 +6,13 @@
         {{-- Add query string and echo of current time to app.css file to force Apache cache reset --}}
         <link rel="stylesheet" href="{{asset('css/app.css')}}?<?php echo date('l jS \of F Y h:i:s A'); ?>">
         <title>{{config('app.name', 'LSAPP')}}</title>
-
-        <!-- Fonts -->
-        
-
-        <!-- Styles -->
-        
+    
     </head>
     <body>
-      @yield('content')
+      @include('inc.navbar')
+      <div class="container">
+          @yield('content')
+      </div>
+      
     </body>
 </html>
