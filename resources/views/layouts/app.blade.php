8bb3ae0b125fcacb160ac2a7aca05a54375412d9
Carl Hain <crhain@gmail.com>
2019-06-07 11:40:29 -0500

    Add reference to app.css file in app.blade.php layout document to bring some css styles in to project; update package.json to run dev packages in package.json

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        {{-- Add query string and echo of current time to app.css file to force Apache cache reset --}}
+        <link rel="stylesheet" href="{{asset('css/app.css')}}?<?php echo date('l jS \of F Y h:i:s A'); ?>">
         <title>{{config('app.name', 'LSAPP')}}</title>
 
         <!-- Fonts -->
