67e5ab5c7317ebaa8334cffe5f69bbb4b92349b9
Carl Hain <crhain@gmail.com>
2019-06-07 12:13:24 -0500

    Add jumbotron formating to index view

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>{{$title}}</h1>
-  <p>This is the laravel application from the "Laravel From Scratch" Youtube series</p>
+<div class="jumbotron text-center">
+    <h1>{{$title}}</h1>
+    <p>This is the laravel application from the "Laravel From Scratch" Youtube series</p>
+    <p>
+      <a href="/login" class="btn btn-primary btn-lg" role="button">Login</a>
+      <a href="/register" class="btn btn-success btn-lg" role="button">Register</a>
+    </p>
+</div>
+  
 @endsection 
