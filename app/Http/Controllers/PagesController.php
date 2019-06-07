9f4b4323560289d1e74e526a455d553107e14ae7
Carl Hain <crhain@gmail.com>
2019-06-07 10:56:45 -0500

    Update services template page to display title and list of services that are passed as data from PagesController

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -21,6 +21,10 @@ class PagesController extends Controller
     }
 
     public function services(){
-        return view('pages.services');
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web Design', 'Programming', 'SEO']
+        );
+        return view('pages.services')->with($data);
     }
 }
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>Services</h1>
-  <p>This is the services page</p>
+  <h1>{{$title}}</h1>
+  @if(count($services) > 0)
+    <ul>
+    @foreach($services as $service)
+      <li>{{$service}}</li>
+    @endforeach
+    </ul>
+  @endif
 @endsection 
 
